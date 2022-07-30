856713060044bafdb8bb9af653bcc46a615cb9fa
mattyding <66889540+mattyding@users.noreply.github.com>
2022-07-30 11:05:24 -0700

    Update sample-bot.py

--- sample-bot.py
+++ sample-bot.py
@@ -13,7 +13,7 @@ import json
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # Replace "REPLACEME" with your team name!
-team_name = "REPLACEME"
+team_name = "FLATWHITE"
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
